dfb348aa658be9d4b0dc1bb1aa5a0dc72b92fa53
Reshma-Dudekula-786 <131864376+Reshma-Dudekula-786@users.noreply.github.com>
2023-06-06 20:31:37 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar --> 
       <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <a class="navbar-brand" href="index.php">SPARKS BANK</a>
+      <a class="navbar-brand" href="index.php">RESHMA BANK</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
